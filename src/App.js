7a52001a42b603cfbe6fe8ac647091b1647a960c
Pratik Suryawanshi <pratiksuryawanshi8902@gmail.com>
2025-01-02 16:03:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Authform from './Authform';
+
+function App() {
+  return (
+ <div>
+  <Authform/>
+ </div>
+  );
+}
+
+export default App;
